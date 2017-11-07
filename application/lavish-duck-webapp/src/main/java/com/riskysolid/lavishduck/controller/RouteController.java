17f141624705d114dc9344a1401c5099181cd3dd
Stephen Legge <stephen@aleggeup.com>
2017-11-07 01:50:12 -0500

    Add missing license header

--- application/lavish-duck-webapp/src/main/java/com/riskysolid/lavishduck/controller/RouteController.java
+++ application/lavish-duck-webapp/src/main/java/com/riskysolid/lavishduck/controller/RouteController.java
@@ -1,3 +1,12 @@
+/*
+ * RouterController.java
+ *
+ * Copyright (C) 2017 Stephen Legge, Tim McCrabb and Andy Redfearn
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
 package com.riskysolid.lavishduck.controller;
 
 import org.springframework.stereotype.Controller;
